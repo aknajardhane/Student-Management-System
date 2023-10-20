31e62afc8c585a078aee738a87e88208204b99b6
aknajardhane <84505385+aknajardhane@users.noreply.github.com>
2023-10-20 13:17:47 +0530

    Add files via upload

---
+++ src/main/java/com/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,33 @@
+package com.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sms.entity.Student;
+import com.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+    @Autowired
+    private StudentRepository studentRepository ;
+    
+	@Override
+	public void run(String... args) throws Exception {
+		
+//		Student student1 = new Student("ak","najar","aknajar@gmail.com");
+//		studentRepository.save(student1);
+//		
+//		Student student2 = new Student("Tony","Singh","tsingh@gmail.com");
+//		studentRepository.save(student2);
+//		
+//		Student student3 = new Student("Amit","Shinde","ashinde@gmail.com");
+//		studentRepository.save(student3);
+	}
+
+}
---
+++ src/main/java/com/sms/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.sms.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sms.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+}
